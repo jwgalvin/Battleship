760147f288709ffcf8486037b6c029dc7eb04965
Luke Pascale <lpluke5@gmail.com>
2021-12-09 17:58:26 -0700

    feat: Initialize ship.rb file

---
+++ lib/board.rb
@@ -0,0 +1,17 @@
+class Board
+attr_reader :rows, :columns
+
+  def initialize(rows, columns)
+    @rows = rows
+    @columns = columns
+    @cells = {}
+  end
+# hash.new{|h,k,v|} h[k] = 0
+  board.new{|rows, columns|} rows[columns] = 0
+  def cells
+    board.each do |board|
+      @cells <<
+    rows = Range.new(1, @rows)
+    columns = Range.new("A", @columns)
+  end
+end
