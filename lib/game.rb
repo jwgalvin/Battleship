fbb9e6b4114a77f36885c1ee75953f5df76bc888
Luke Pascale <lpluke5@gmail.com>
2021-12-11 19:23:52 -0700

    feat: Add main_menu functionality and game begin

--- lib/game.rb
+++ lib/game.rb
@@ -3,36 +3,41 @@ require './lib/cell.rb'
 require './lib/ship.rb'
 
 class Game
-  # attr_reader
+  #attr_reader :navy_placement
   #def initialize(new_game)
   #  @new_game = new_game
   #end
 
-  def start
-    #p "This statement"
+  def main_menu
+    puts "Welcome to BATTLESHIP \n Enter p to play. Enter q to quit."
     return "Welcome to BATTLESHIP \n Enter p to play. Enter q to quit."
-    #user_input = gets.chomp
-
-    #if user_input == "p" #this is our main branch and idea where the game will be rendered and started.
-      # game.new
-      #p "New game"
-    #elsif user_input == "q"
-      #p "Quit"
-    #else
-      #p "Invalid"
-    #end
+    user_input
   end
 
-  def user_input
+  def setup
     user_input = gets.chomp
 
     if user_input == "p" #this is our main branch and idea where the game will be rendered and started.
-      # game.new
-      p "New game"
+      p "Welcome to the Navy"
+      new
     elsif user_input == "q"
       p "Quit"
     else
       p "Invalid"
+    end
+
+    def begin_game
+      prints "You will have 2 vessels to your Fleet. A CruiserklLorem ipsum oloseontg. Put the instructions to the game here"
+      board = Board.new(4, 4)#(user_input, user_input)
+
+    end
+    def turn
+      # this will dictate when the game ends as well
+      # until method should be used here until both ships are sunk for either player
+      # computer placement will choose from ~8 choices, when a random choice is decided, a specific setup will be engaged.
+      # computer shot will check to see if it has hit anything prior? selects from 8 different patterns of choice.
+      # computer.choice(1..8) random do ==> 4 C1 C2 C3 C4 D1 D2
+      #
     end
   end
 end
