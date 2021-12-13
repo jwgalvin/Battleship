2ad69d6e2bc6bc6be26de7da4a87e7259defb750
Joseph Galvin <jwgalvin@gmail.com>
2021-12-13 11:18:56 -0700

    feat: working on text for directions

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -2,6 +2,6 @@ require './lib/board.rb'
 require './lib/cell.rb'
 require './lib/ship.rb'
 require './lib/game.rb'
-
+require 'pry'
 @warfare = Game.new
 @warfare.main_menu
--- lib/game.rb
+++ lib/game.rb
@@ -6,14 +6,13 @@ class Game
   # attr_reader :begin_game
 
   def initialize
-    @boards = []
     @player_board = Board.new
     @computer_board = Board.new
   end
 
   def main_menu
     # This is how we begin a game, can we clear the screen first?
-    # reset
+    system "clear"
     puts "Welcome to BATTLESHIP \nEnter p to play. Enter q to quit."
     user_input = gets.chomp.downcase
 
@@ -25,7 +24,7 @@ class Game
       p "Welcome to the Navy"
       begin_game
       elsif user_input == "q"
-          p "Quit"
+          p "Quitter"
       else
 
           # exit ??? can we clear the screen like we can with terminal
@@ -34,18 +33,20 @@ class Game
   end
 
     def begin_game
+      system "clear"
       directions = "You will have 2 vessels to your Fleet. \nCruiser is 3 squares. \nSubmarine is 2 squares. \nThe Game board itself is a square. Its default size is 4 x 4. \nEach turn will consist  of you, then skynet, choosing a location to fire upon.  The game will continue until you or skynet is doomed to the bottom of the digital sea. Ships must be placed in a straight line! Place your Navy wisely!!! For all Mankind!"
-      @boards = []
-      boards << player_board = Board.new
-      boards << computer_board = Board.new
-      puts "#{boards[0].render(true)}"
-      puts "Place your cruiser on the Board now, it will need 3 coordinates that are in a straight line. Please choose them one at a time."
-      cruiser << get.chomp.upcase!
-      #for cruiser.each  do |placement|
-        #@boards[0].place(cruiser, )
-        #let(:player_board) {Board.new}#(user_input, user_input)
-        #let(:computer_board) {Board.new}
-      #end
+      # puts directions
+      @boards << player_board = Board.new
+      @boards << computer_board = Board.new
+      ship_1 = Ship.new("Cruiser", 3)
+      ship_coords = []
+      puts "#{@boards[0].render(true)}"
+      puts "Place your cruiser on the Board now, it will need 3 coordinates that are in a straight line. Please choose them, separate them with a comma."
+      ship_coords << gets.chomp.tr(",", " ").upcase!
+      # ship_coords.each do |placement|
+      player_board.place(ship_1, ship_coords)
+      # end
+      puts "#{@boards[0].render(true)}"
     end
 
     #def turn
@@ -56,4 +57,5 @@ class Game
       # computer.choice(1..8) random do ==> 4 C1 C2 C3 C4 D1 D2
       #
     #end
+    # binding.pry
 end
